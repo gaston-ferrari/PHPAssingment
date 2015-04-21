8b282c9f75c5d351ea2f8aab19f83d12399ac100
Gaston Ferrari <gastonferrari5@gmail.com>
2015-04-21 00:56:03 -0300

    changed index to create only the controller being used

--- index.php
+++ index.php
@@ -6,16 +6,15 @@ require_once(__DIR__  . '/Routes.php');
 $req = new \Api\Util\Request();
 $routes = new Routes();
 
-$addressController = new \Api\Controller\AddressController();
-
-$routes->get("/address", array($addressController, 'showAddress'));
-$routes->post("/address", array($addressController, 'addAddress'));
-$routes->del("/address", array($addressController, 'deleteAddress'));
-$routes->put("/address", array($addressController, 'updateAddress'));
+$routes->get("/address", array('Api\Controller\AddressController', 'showAddress'));
+$routes->post("/address", array('Api\Controller\AddressController', 'addAddress'));
+$routes->del("/address", array('Api\Controller\AddressController', 'deleteAddress'));
+$routes->put("/address", array('Api\Controller\AddressController', 'updateAddress'));
 
 try{
     $handler = $routes->getHandler($req->getPath(), $req->getType());
-    $res = call_user_func($handler, $req);
+    $controller = new $handler[0];
+    $res = $controller->$handler[1]($req);
     echo json_encode($res);
 } catch (Exception $ex) {
     http_response_code(500);
